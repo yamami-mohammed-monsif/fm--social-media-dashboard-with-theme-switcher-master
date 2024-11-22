35e51c206c82087d0fc6ab2e38433a8966a7f216
yamami mohamed monsif <mohammedyamami05@gmail.com>
2024-11-22 18:50:08 +0000

    updated the code so the theme will be stored in the local storage so it won't change when the page is reloaded or reopened

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-const toggleButton = document.getElementById('toggle-theme-btn');
+const toggleButton = document.getElementById("toggle-theme-btn");
+const body = document.querySelector("body");
 
-toggleButton.addEventListener('click', () => {
-
-    document.querySelector('body').classList.toggle('dark');
-    toggleButton.classList.toggle('active');
+window.addEventListener("load", () => {
+  // when opening the app fist time the theme in local storage will be null, default it to light
+  const theme = localStorage.getItem("theme") || "light";
+  body.className = theme;
 });
+
+function changeTheme() {
+  // change the theme
+  if (body.className === "light") {
+    body.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    body.classList.remove("dark");
+    localStorage.setItem("theme", "light");
+  }
+  // make the button move
+  toggleButton.classList.toggle("active");
+}
+
+toggleButton.addEventListener("click", changeTheme);
